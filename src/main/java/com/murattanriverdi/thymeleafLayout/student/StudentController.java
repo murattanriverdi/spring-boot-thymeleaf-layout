0b23c84bce15345ece42bc32039b1b631e40c103
Murat <murat@Murat-MacBook-Pro.local>
2022-07-18 23:17:21 +0300

    create thymeleaf layout

---
+++ src/main/java/com/murattanriverdi/thymeleafLayout/student/StudentController.java
@@ -0,0 +1,17 @@
+package com.murattanriverdi.thymeleafLayout.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping
+    public String index(){
+
+        return "student/index";
+    }
+
+}
---
+++ src/test/java/com/murattanriverdi/thymeleafLayout/LadpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murattanriverdi.thymeleafLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
